cc830ad9ed1f74d57385996405efc37cfb5eaadb
George <george.hamann@gmail.com>
2022-07-18 17:19:31 -0300

    Feat: added base project

---
+++ Authentication.Infrastructure/Extension/InfrastructureExtension.cs
@@ -0,0 +1,15 @@
+﻿using Authentication.Domain.Repositories;
+using Authentication.Infra;
+
+namespace Authentication.Infrastructure.Extension
+{
+    public static class InfrastructureExtension
+    {
+        public static void AddInfraExtensions(this IServiceCollection services, IConfiguration configuration )
+        {
+            services.AddSingleton<IUserRepository, UserRepository>();
+            services.Configure<Settings>(configuration.GetSection("Settings"));
+
+        }
+    }
+}
---
+++ Authentication.Service/Controllers/UsersController.cs
@@ -0,0 +1,70 @@
+﻿using Authentication.Application.UserModule;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Authentication.Controllers
+{
+    [Route("api/Users")]
+    [ApiController]
+    //[Authorize]
+    public class UsersController : ControllerBase
+    {
+        private IUserService _userService;
+
+        public UsersController(IUserService userService)
+        {
+            this._userService = userService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllAsync()
+        {
+            var users = await _userService.GetAll();
+            if(users != null)
+                return Ok(users);
+            return BadRequest();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(UserModel userCreateRequest)
+        {
+            string error =  await _userService.Create(userCreateRequest);
+            if (String.IsNullOrEmpty(error))
+            {
+                    return Ok();                
+            }
+            return BadRequest(error);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(UserModel userUpdateRequest)
+        {
+            string error = await _userService.ValidateUserData(userUpdateRequest);
+            if (String.IsNullOrEmpty(error))
+            {
+                //UserModel user = _userService.UserMap(userUpdateRequest);
+                //error = await _userService.Update(user);
+                if (String.IsNullOrEmpty(error))
+                {
+                    return Ok();
+                }
+            }
+
+            return BadRequest(error);
+        }       
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            string error = await _userService.CheckIfIdExist(id);
+            if (String.IsNullOrEmpty(error))
+            {
+                error = await _userService.Delete(id);
+                if (String.IsNullOrEmpty(error))
+                {
+                    return Ok();
+                }
+           }
+            return BadRequest(error);
+        }
+    }
+}
